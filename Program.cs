b228924cbe25c79c30eb3e05fafb44e8d5943218
Cole Henderson <darkgale93@gmail.com>
2022-05-06 17:55:02 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class MyProgram
+{
+    public static void GetName()
+    {
+        Console.WriteLine("Hello, what is your name?");
+        string myname = Console.ReadLine();
+        if (myname == "cole")
+        {
+            Console.WriteLine("That's me!");
+        }
+        else
+        {
+            Console.WriteLine($"Hello,{myname}!");
+        }
+    }
+    public static void GetAge()
+    {
+        Console.WriteLine("What is your age?");
+        string age = Console.ReadLine();
+        int numAge = Convert.ToInt32(age);
+        if (numAge < 18)
+        {
+            Console.WriteLine($"Your {age} years young!");
+        }
+        else
+        {
+            Console.WriteLine($"Your {age} years not young!");
+        }
+    }
+    public static void Main()
+    {
+        GetName();
+        GetAge();
+        Console.ReadKey();
+    }
+}
